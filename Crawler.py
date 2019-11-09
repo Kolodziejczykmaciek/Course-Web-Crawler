f7966801c98831c65c5a168f5a75ad4b2daa43f5
Kolodziejczykmaciek <maciekk529@gmail.com>
2019-11-09 16:06:01 +0100

    first commit

---
+++ Crawler.py
@@ -0,0 +1,17 @@
+import requests
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+target_url = "google.com"
+
+with open("subdomains-wodlist.txt", "r") as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()                     #delete the "/n" at the end of each line
+        test_url = word + "." + target_url
+        response = request(test_url)
+        if response:
+            print("[+] Discoverd subdomain --> " + test_url )
